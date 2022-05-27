c2f640b210d07dd845baf749d7663865c50e20bd
dmitriy_martys <12masek34@mail.ru>
2022-05-27 11:41:17 +0300

    add swagger schema.

--- blog/urls.py
+++ blog/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from .swagger import schema_view
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('accounts_app.urls')),
-    path('api/v1/', include('blog_app.urls'))
+    path('api/v1/', include('blog_app.urls')),
+
+    path('docs/yml/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
